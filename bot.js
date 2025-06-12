71b2380a291422f6846ce8ac705a38c0b9a63785
maksalina-shop <magazinmaksalina@gmail.com>
2025-06-12 11:58:21 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Бот работает!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
